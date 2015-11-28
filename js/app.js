340334d03150be2f752a2e59f99f72d60d77c8ed
Leonardo Santos <lsantos@inoa.com.br>
2015-11-27 22:28:14 -0200

    correcao do dia

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ angular
 .module('WeddingSite', [])
 .controller('WeddingIndexCtrl',
 function ($scope) {
-  $scope.remaining_days = moment('2015-12-06').diff(moment(), 'days');
+  $scope.remaining_days = moment('2015-12-06').diff(moment(), 'days')+1;
 });
