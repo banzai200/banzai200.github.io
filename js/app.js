a6a4cc13753835a51ec6af9926df742a81b6842e
Wesley Putumuju <wesley.putumuju.ext@jumia.com>
2022-06-29 18:45:09 -0300

    Edited everything

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ angular
 .module('WeddingSite', [])
 .controller('WeddingIndexCtrl',
 function ($scope) {
-  $scope.remaining_days = moment('2015-12-06').diff(moment(), 'days')+1;
+  $scope.remaining_days = moment('2022-07-16').diff(moment(), 'days')+1;
 });
